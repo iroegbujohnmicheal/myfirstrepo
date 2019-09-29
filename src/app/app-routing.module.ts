c0be77e40d1ceb624a3172e253708cba1bb69f3f
John Micheal <diamonds1759@gmail.com>
2019-09-29 21:16:46 +0100

    firstNodeCommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './posts/posts/posts.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'posts'},
+  { path: 'posts', component: PostsComponent},
+  { path: 'addPosts', component: AddPostComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
